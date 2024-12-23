4c9fae3c79b1e846a354570b6c826417e0c94e6f
potakaaa <helbirog@gmail.com>
2024-12-23 19:55:18 +0800

    working dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <div className="size-full bg-background h-screen text-white flex justify-center items-center space-x-2">
         <ModeToggle />
         <h2 className="scroll-m-20 text-lg font-semibold tracking-tight text-foreground transition-all duration-300">
